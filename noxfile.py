862750fdbcec2f99bae5ed0895af77512bf441e3
Janos Tolgyesi <janos.tolgyesi@neosperience.com>
2023-11-09 13:22:54 +0100

    More fix in coverage fail-under

--- noxfile.py
+++ noxfile.py
@@ -174,7 +174,8 @@ def tests(session: Session) -> None:
 @session(python=python_versions[0])
 def coverage(session: Session) -> None:
     """Produce the coverage report."""
-    args = session.posargs or ["report", "--fail-under=0"]
+    args = session.posargs or ["report"]
+    args += ["--fail-under=0"]
 
     session.install("coverage[toml]")
 
