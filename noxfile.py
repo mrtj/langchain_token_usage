59872d57c240399742a3e2434e0cd3e90be64fd2
Janos Tolgyesi <janos.tolgyesi@neosperience.com>
2023-11-09 13:04:22 +0100

    Add boto3 stubs to reqs, ignore langchain safety error

--- noxfile.py
+++ noxfile.py
@@ -143,7 +143,9 @@ def safety(session: Session) -> None:
     """Scan dependencies for insecure packages."""
     requirements = session.poetry.export_requirements()
     session.install("safety")
-    session.run("safety", "check", "--full-report", f"--file={requirements}")
+    session.run(
+        "safety", "check", "--full-report", f"--file={requirements}", "--ignore=60433"
+    )
 
 
 @session(python=python_versions)
